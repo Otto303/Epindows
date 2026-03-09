a257540ee87af0a126295fb74c42d3c562035bf8
ilyann.gwinner <ilyann.gwinner@gmail.com>
2026-03-09 18:38:56 +0100

    fix: installation

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo Installing configs...
 git clone https://github.com/Otto303/Epindows.git $files -b dev &>/dev/null
 
 mv ~/.config/i3/config /tmp/config.bak
-cp $sys32/config/System32 ~/.config/i3/config
+cp $sys32/i3/config ~/.config/i3/config
 i3-msg restart; sleep 1
 mv /tmp/config.bak ~/.config/i3/config
 
