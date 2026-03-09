2a51567a7b9179d931b32452554197f699140543
Otto303 <ottodebrie@gmail.com>
2026-03-09 17:27:18 +0100

    add install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+i3-msg floating enable
+
 echo Installing packages...
 ./pkgs.sh > /dev/null
 
 echo Openning desktop...
 pcmanfm --desktop & disown
+i3-msg floating disable; sleep 1
 i3-msg focus right floating disable border none
-echo Activating GUI...
-tint2 -c ~/afs/win_test/taskbar_xp & disown
-rofi -show drun -config ~/afs/win_test/config.rasi & disown
+i3-msg floating enable
 
-exit
+echo Activating GUI...
+(tint2 -c ~/afs/win_test/taskbar_xp & disown) > /dev/null
+(rofi -show drun -config ~/afs/win_test/config.rasi & disown) > /dev/null
+
+echo Epindows Successfully installed!
